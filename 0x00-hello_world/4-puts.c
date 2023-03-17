d9264a8e657636cbf4e3a48839804eab1fa4ca1c
jodeb10 <jodebcity@gmail.com>
2023-03-16 17:02:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,3 +2,8 @@
 /**
  * main - a program that print a line with puts function * Return: 0 (Success)
  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+*return (0);
+}
